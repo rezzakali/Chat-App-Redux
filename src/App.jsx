4c54f67b37af08ff341faef95f8c9449242f949d
rezzak ali <rezzak ali>
2023-04-12 11:29:17 +0530

    chat app scaffolding code commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Conversation from './pages/Conversation';
+import Inbox from './pages/Inbox';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Error({ message }) {
+  return (
+    <div className="flex items-center">
+      <div className="relative bg-red-200 max-w-xl px-4 py-2 text-red-800 rounded shadow w-full">
+        <span className="block text-sm">{message}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
