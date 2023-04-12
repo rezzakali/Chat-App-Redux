93c01d5466084f1fdbaabd2026950904350df1a0
rezzak ali <rezzak ali>
2023-04-12 15:24:49 +0530

    authenticated request code push

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,13 @@ const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_PORT,
+    prepareHeaders: async (headers, { getState, endpoints }) => {
+      const token = getState()?.auth?.accessToken;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
