0f84806ba4c65266fdee84783d62a6e280a3d083
rezzak ali <rezzak ali>
2023-04-15 17:09:17 +0530

    websocket implementation json server change code push

--- server/server.js
+++ server/server.js
@@ -1,24 +1,33 @@
-const auth = require("json-server-auth");
-const jsonServer = require("json-server");
+import express from 'express';
+import http from 'http';
+import jsonServer from 'json-server';
+import auth from 'json-server-auth';
+import { Server } from 'socket.io';
 
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const app = express();
+const server = http.createServer(app);
+let io;
+io = new Server(server);
+
+global.io = io;
+
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
 // Bind the router db to the app
-server.db = router.db;
+app.db = router.db;
 
-server.use(middlewares);
+app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
-server.use(rules);
-server.use(auth);
-server.use(router);
+app.use(rules);
+app.use(auth);
+app.use(router);
 
 server.listen(port);
