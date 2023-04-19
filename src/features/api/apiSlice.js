b01f79640d99148529a0aa4ed6f9b03f8495866d
rezzak ali <rezzak ali>
2023-04-19 17:30:15 +0530

    basequery customize code push

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,17 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const baseQueryWithReauth = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_PORT,
+  prepareHeaders: async (headers, { getState, endpoints }) => {
+    const token = getState()?.auth?.accessToken;
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
 const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_PORT,
-    prepareHeaders: async (headers, { getState, endpoints }) => {
-      const token = getState()?.auth?.accessToken;
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: async (args, api, extraOptions) => {
+    let result = await baseQueryWithReauth(args, api, extraOptions);
+    if (result?.error && result.error.status === 401) {
+      api.dispatch(userLoggedOut());
+      localStorage.clear();
+    }
+    return result;
+  },
   tagTypes: [],
   endpoints: (builder) => ({}),
 });
